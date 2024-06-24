817d9ac2becc02a2764731c18bc328d850e0d68e
Jason Morley <hello@jbmorley.co.uk>
2024-06-23 20:56:13 -1000

    feat: Add a sidebar for browsing different locations
    Right now this is hard-coded to only show C.

--- Reconnect/Views/BrowserView.swift
+++ Reconnect/Views/BrowserView.swift
@@ -22,51 +22,67 @@ struct BrowserView: View {
 
     @State var model = BrowserModel()
 
+    @State var selection: Int = 1
+
     var body: some View {
-        VStack {
-            switch model.state {
-            case .loading:
-                Text("Loading...")
-            case .ready(let files):
-                Table(files, selection: $model.selection) {
-                    TableColumn("") { file in
-                        if file.attributes.contains(.directory) {
-                            let image = NSWorkspace.shared.icon(for: .folder)
-                            Image(nsImage: image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 16.0)
-                        } else {
-                            Image("FileUnknown")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 16.0)
+        NavigationSplitView {
+            List(selection: $selection) {
+                Section("Drives") {
+                    Label {
+                        Text("Internal (C:)")
+                    } icon: {
+                        Image(systemName: "internaldrive")
+                    }
+                    .tag(1)
+                }
+            }
+        } detail: {
+            VStack {
+                switch model.state {
+                case .loading:
+                    Text("Loading...")
+                case .ready(let files):
+                    Table(files, selection: $model.selection) {
+                        TableColumn("") { file in
+                            if file.attributes.contains(.directory) {
+                                let image = NSWorkspace.shared.icon(for: .folder)
+                                Image(nsImage: image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 16.0)
+                            } else {
+                                Image("FileUnknown")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 16.0)
+                            }
+                        }
+                        .width(16.0)
+                        TableColumn("Name", value: \.name)
+                        TableColumn("Size") { file in
+                            Text(String(file.size))
                         }
                     }
-                    .width(16.0)
-                    TableColumn("Name", value: \.name)
-                    TableColumn("Size") { file in
-                        Text(String(file.size))
-                    }
-                }
-                .contextMenu(forSelectionType: FileServer.DirectoryEntry.ID.self) { items in
-                    Button("Hello, World!") {
+                    .contextMenu(forSelectionType: FileServer.DirectoryEntry.ID.self) { items in
+                        Button("Hello, World!") {
 
+                        }
+                    } primaryAction: { items in
+                        guard
+                            items.count == 1,
+                            let item = items.first
+                        else {
+                            return
+                        }
+                        print(item)
+                        // TODO: Differentiate between files and items.
+                        model.load(path: item)
                     }
-                } primaryAction: { items in
-                    guard
-                        items.count == 1,
-                        let item = items.first
-                    else {
-                        return
-                    }
-                    print(item)
-                    // TODO: Differentiate between files and items.
-                    model.load(path: item)
+                case .error(let error):
+                    Text(String(describing: error))
                 }
-            case .error(let error):
-                Text(String(describing: error))
             }
+
         }
         .toolbar {
             ToolbarItem(placement: .navigation) {
