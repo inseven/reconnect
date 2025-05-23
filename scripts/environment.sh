cf2eb11585b0297d56404487878c46c5383e46a0
Jason Morley <hello@jbmorley.co.uk>
2025-05-23 10:47:16 -1000

    fix: 📃 Update the copyright to 2025 (#172)

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -2,7 +2,7 @@
 
 # Reconnect -- Psion connectivity for macOS
 #
-# Copyright (C) 2024 Jason Morley
+# Copyright (C) 2024-2025 Jason Morley
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
