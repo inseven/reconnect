c589fdc680b90e1eca51c96558e97525f8c651c0
Jason Morley <hello@jbmorley.co.uk>
2025-06-02 10:00:14 -1000

    Move `TextQueryInstallerPage` into a separate file

---
+++ Reconnect/Views/TextQueryInstallerPage.swift
@@ -0,0 +1,58 @@
+// Reconnect -- Psion connectivity for macOS
+//
+// Copyright (C) 2024 Jason Morley
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+import SwiftUI
+
+@MainActor
+struct TextQueryInstallerPage: View {
+
+    let query: InstallerModel.TextQuery
+
+    var body: some View {
+        InstallerPage(query.installer.localizedDisplayName) {
+            ScrollView {
+                Text(query.text)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+            }
+            .background(Color(nsColor: .textBackgroundColor))
+        } actions: {
+            switch query.type {
+            case .continue:
+                Button("Continue") {
+                    query.continue(true)
+                }
+                .keyboardShortcut(.defaultAction)
+            case .skip, .abort:
+                Button("No") {
+                    query.continue(false)
+                }
+                Button("Yes") {
+                    query.continue(true)
+                }
+                .keyboardShortcut(.defaultAction)
+            case .exit:
+                Button("Exit") {
+                    query.continue(true)
+                }
+                .keyboardShortcut(.defaultAction)
+            }
+        }
+    }
+
+}
