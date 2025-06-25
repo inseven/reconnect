368b5beaf891caa435f40729b0221baa51d82b43
Jason Morley <hello@jbmorley.co.uk>
2025-06-24 15:34:34 -1000

    Proof of concept build with the latest update

--- dependencies/plptools/Package.swift
+++ dependencies/plptools/Package.swift
@@ -30,27 +30,56 @@ let package = Package(
     dependencies: [],
     targets: [
         .target(
-            name: "plptools",
+            name: "libgnu",
+            dependencies: [],
+            path: "plptools",
+            sources: [
+                "libgnu/yesno.c",
+                "libgnu/string-buffer.c",
+            ]
+        ),
+        .target(
+            name: "core",
             dependencies: [],
             path: "plptools",
             exclude: [
                 "lib/Makefile.am",
+                "lib/Makefile.in",
+                "lib/Makefile",
             ],
             sources: [
                 "lib",
+                // "gnulib/lib",
+                // "libgnu/yesno.c",  // TODO: Work out which one is in the source root.
             ],
-            publicHeadersPath: "lib"
+            publicHeadersPath: "lib",
+            cSettings: [
+                // .headerSearchPath("libgnu"),
+                // .headerSearchPath("libgnu/lib"),
+                .unsafeFlags(["-Wno-int-conversion"]),
+                // .unsafeFlags(["-include", "config.h"]),  // TODO: We should be able to apply this to every target?
+            ],
+            // cxxSettings: [  // TODO: ChatGPT recommended this so I don't trust it.
+            //     .headerSearchPath("libgnu"),
+            //     .headerSearchPath("libgnu/lib"),
+            //     .unsafeFlags(["-Wno-int-conversion"]),
+            //     .unsafeFlags(["-include", "config.h"]),  // TODO: We should be able to apply this to every target?
+            //     .unsafeFlags(["-std=c++17"])
+            // ],
         ),
         .target(
             name: "ncp",
             dependencies: [
-                "plptools",
+                "core",
+                "libgnu",
             ],
             path: "plptools",
             exclude: [
-                // "ncpd/main.cc",
+                "ncpd/main.cc",  // We have to rename this because Swift has special magic for 'main'.
                 // "ncpd/main.h",
                 "ncpd/Makefile.am",
+                "ncpd/Makefile.in",
+                "ncpd/Makefile",
             ],
             sources: [
                 "ncpd",
@@ -58,18 +87,24 @@ let package = Package(
             publicHeadersPath: "ncpd",
             cSettings: [
                 .headerSearchPath("lib"),  // TODO: Put the config in an extra directory here?
+                .headerSearchPath("gnulib/lib"),
+                .unsafeFlags(["-Wno-int-conversion"]),
+                // .unsafeFlags(["-include", "config.h"]),
             ]
             // swiftSettings: [.interoperabilityMode(.Cxx)]
         ),
-        .target(
+        .target(  // TODO: Why do I need this?
             name: "plpftp",
             dependencies: [
-                "plptools",
+                "core",
             ],
             path: "plptools",
             exclude: [
                 "plpftp/Makefile.am",
+                "plpftp/Makefile.in",
+                "plpftp/Makefile",
                 "plpftp/main.cc",
+                "plpftp/ftp.cc",
             ],
             sources: [
                 "plpftp"
@@ -77,6 +112,9 @@ let package = Package(
             publicHeadersPath: "plpftp",
             cSettings: [
                 .headerSearchPath("lib"),  // TODO: Put the config in an extra directory here?
+                .headerSearchPath("gnulib/lib"),
+                .unsafeFlags(["-Wno-int-conversion"]),
+                // .unsafeFlags(["-include", "config.h"]),
             ]
         )
     ]
