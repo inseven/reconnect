f4e97c4acfe1ce6203c7504e9fa3ac3d95178f2c
Jason Morley <hello@jbmorley.co.uk>
2025-07-09 10:48:22 -1000

    refactor: Clean up the website build scripts (#260)
    I think these are leftovers from the StatusPanel build script.
    
    This change also includes some drive-by code cleanup and simplification
    in the website build script.

--- scripts/build-website.sh
+++ scripts/build-website.sh
@@ -27,28 +27,20 @@ SCRIPTS_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd 
 
 ROOT_DIRECTORY="$SCRIPTS_DIRECTORY/.."
 WEBSITE_DIRECTORY="$ROOT_DIRECTORY/docs"
-WEBSITE_SIMULATOR_DIRECTORY="$ROOT_DIRECTORY/docs/simulator"
-SIMULATOR_WEB_DIRECTORY="$ROOT_DIRECTORY/simulator/web"
-
-RELEASE_NOTES_TEMPLATE_PATH="$SCRIPTS_DIRECTORY/release-notes.md"
-HISTORY_PATH="$SCRIPTS_DIRECTORY/history.yaml"
-RELEASE_NOTES_DIRECTORY="$ROOT_DIRECTORY/docs/release-notes"
-RELEASE_NOTES_PATH="$RELEASE_NOTES_DIRECTORY/index.markdown"
 
 source "$SCRIPTS_DIRECTORY/environment.sh"
 
 cd "$ROOT_DIRECTORY"
-if [ -d "$RELEASE_NOTES_DIRECTORY" ]; then
-    rm -r "$RELEASE_NOTES_DIRECTORY"
-fi
+
+# Update the release notes.
 "$SCRIPTS_DIRECTORY/update-release-notes.sh"
 
 # Install the Jekyll dependencies.
-export GEM_HOME="${ROOT_DIRECTORY}/.local/ruby"
+export GEM_HOME="$ROOT_DIRECTORY/.local/ruby"
 mkdir -p "$GEM_HOME"
-export PATH="${GEM_HOME}/bin":$PATH
+export PATH="$GEM_HOME/bin":$PATH
 gem install bundler
-cd "${WEBSITE_DIRECTORY}"
+cd "$WEBSITE_DIRECTORY"
 bundle install
 
 # Get the latest release URL.
@@ -61,8 +53,8 @@ if [[ -z "$DOWNLOAD_URL" ]]; then
     echo "Failed to get release download URL."
     exit 1
 fi
+export DOWNLOAD_URL
 
 # Build the website.
-cd "${WEBSITE_DIRECTORY}"
-export DOWNLOAD_URL
+cd "$WEBSITE_DIRECTORY"
 bundle exec jekyll build
