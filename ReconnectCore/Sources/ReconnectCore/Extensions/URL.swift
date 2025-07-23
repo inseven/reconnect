d26d8c698674322a2e35624e8445d0780ee66241
Jason Morley <hello@jbmorley.co.uk>
2025-07-23 08:37:01 -1000

    fix: 🔗 Add website and privacy policy links (#282)

--- ReconnectCore/Sources/ReconnectCore/Extensions/URL.swift
+++ ReconnectCore/Sources/ReconnectCore/Extensions/URL.swift
@@ -29,10 +29,11 @@ extension URL {
     public static let psionSoftwareIndex = URL(string: "x-reconnect://psion-software-index")!
     public static let update = URL(string: "x-reconnect://update")!
 
-    public static let discord = URL(string: "https://discord.gg/ZUQDhkZjkK")!
     public static let donate = URL(string: "https://jbmorley.co.uk/support")!
     public static let gitHub = URL(string: "https://github.com/inseven/reconnect")!
+    public static let privacyPolicy = URL(string: "https://reconnect.jbmorley.co.uk/privacy-policy")!
     public static let software = URL(string: "https://jbmorley.co.uk/software")!
+    public static let website = URL(string: "https://reconnect.jbmorley.co.uk")!
 
     public static var support: URL = {
         let subject = "Reconnect Support (\(Bundle.main.extendedVersion ?? "Unknown Version"))"
