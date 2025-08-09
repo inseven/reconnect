0424a8f8ff41f14e8550c967bc980c223ea45a36
Jason Morley <hello@jbmorley.co.uk>
2025-08-09 10:10:57 -1000

    Show a colon after drive letters

--- ReconnectCore/Sources/ReconnectCore/Extensions/DriveInfo.swift
+++ ReconnectCore/Sources/ReconnectCore/Extensions/DriveInfo.swift
@@ -22,7 +22,7 @@ extension FileServer.DriveInfo {
 
     public var displayName: String {
         if let name {
-            return "\(name) (\(drive))"
+            return "\(name) (\(drive):)"
         } else {
             return "\(drive):"
         }
