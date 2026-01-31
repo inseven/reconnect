b38ef040ef9f7d59be69e601a65b76e5738123ed
Jason Morley <hello@jbmorley.co.uk>
2026-01-31 11:38:23 -1000

    fix: 🗓️ Update the copyright notices to 2026

--- ReconnectCore/Sources/ReconnectCore/Extensions/NSRunningApplication.swift
+++ ReconnectCore/Sources/ReconnectCore/Extensions/NSRunningApplication.swift
@@ -1,6 +1,6 @@
 // Reconnect -- Psion connectivity for macOS
  //
- // Copyright (C) 2024-2025 Jason Morley
+ // Copyright (C) 2024-2026 Jason Morley
  //
  // This program is free software; you can redistribute it and/or modify
  // it under the terms of the GNU General Public License as published by
