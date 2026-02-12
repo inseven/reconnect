885b0261e005bfd406d585f5182db1f8cb0a5d47
Jason Morley <hello@jbmorley.co.uk>
2026-02-11 16:35:13 -1000

    fix: ℹ️ Show supported file conversions in Settings (#432)

--- macos/Reconnect/Model/FileConverter.swift
+++ macos/Reconnect/Model/FileConverter.swift
@@ -23,15 +23,150 @@ import OpoLuaCore
 import ReconnectCore
 import Word2text
 
+enum FileType: String, Identifiable {
+
+    var id: Self {
+        return self
+    }
+
+    case mbm
+    case word
+    case text
+    case markdown
+
+}
+
+extension FileType {
+
+    var localizedDescription: LocalizedStringKey {
+        switch self {
+        case .mbm:
+            return "Multiple Bitmap Image (.mbm)"
+        case .word:
+            return "EPOC16 Word (.wrd)"
+        case .text:
+            return "Text (.txt)"
+        case .markdown:
+            return "Markdown (.md)"
+        }
+    }
+
+}
+
+extension FileType {
+
+    func matches(directoryEntry: FileServer.DirectoryEntry) -> Bool {
+        switch self {
+        case .mbm:
+            return directoryEntry.fileType == .mbm || directoryEntry.pathExtension.lowercased() == "mbm"
+        case .word:
+            return directoryEntry.pathExtension.lowercased() == "wrd"
+        case .text:
+            return directoryEntry.pathExtension.lowercased() == "txt"
+        case .markdown:
+            return directoryEntry.pathExtension.lowercased() == "md"
+        }
+    }
+
+}
+
+enum ConversionIdentifier: String {
+
+    case none
+    case mbmToTiff
+    case wordToText
+    case windowsAsciiToUnixUnicode
+
+}
+
+extension ConversionIdentifier {
+
+    fileprivate var conversion: Conversion {
+        switch self {
+        case .none:
+            return .none
+        case .mbmToTiff:
+            return .mbmConversion
+        case .wordToText:
+            return .wordConversion
+        case .windowsAsciiToUnixUnicode:
+            return .textConversion
+        }
+    }
+
+    // TODO: Move this into the conversion?
+    var localizedDescription: LocalizedStringKey {
+        switch self {
+        case .none:
+            return "None"
+        case .mbmToTiff:
+            return "TIFF"
+        case .wordToText:
+            return "Text"
+        case .windowsAsciiToUnixUnicode:
+            return "UTF8 with Unix Line Endings"
+        }
+    }
+
+}
+
+fileprivate struct Conversion {
+    let filename: (FileServer.DirectoryEntry) -> String
+    let perform: (URL, URL) throws -> URL
+}
+
+
+extension Conversion {
+
+    fileprivate static let none: Self = Self { entry in
+        return entry.name
+    } perform: { sourceURL, destinationURL in
+        return sourceURL
+    }
+
+    fileprivate static let mbmConversion: Self = Self { entry in
+        return entry.name
+            .deletingPathExtension
+            .appendingPathExtension("tiff")
+    } perform: { sourceURL, destinationURL in
+        let outputURL = destinationURL.appendingPathComponent(sourceURL.lastPathComponent.deletingPathExtension,
+                                                              conformingTo: .tiff)
+        try PsiLuaEnv().convertMultiBitmap(at: sourceURL, to: outputURL)
+        try FileManager.default.removeItem(at: sourceURL)
+        return outputURL
+    }
+
+    fileprivate static let wordConversion: Self = Self { entry in
+        return entry
+            .name
+            .deletingPathExtension
+            .appendingPathExtension("txt")
+    } perform: { sourceURL, destinationURL in
+        let outputURL = destinationURL.appendingPathComponent(sourceURL.lastPathComponent.deletingPathExtension,
+                                                              conformingTo: .plainText)
+        let data = try Data(contentsOf: sourceURL)
+        let output = try PsionWord.processFile(data).get()
+        try output.write(to: outputURL, atomically: true, encoding: .utf8)
+        return outputURL
+    }
+
+    fileprivate static let textConversion: Self = Self { entry in
+        return entry.name
+    } perform: { sourceURL, destinationURL in
+        let data = try Data(contentsOf: sourceURL)
+        guard let contents = String(data: data, encoding: .ascii) else {
+            throw ReconnectError.unknown
+        }
+        let output = contents.replacingOccurrences(of: "\r\n", with: "\n")
+        try output.write(to: sourceURL, atomically: true, encoding: .utf8)
+        return sourceURL
+    }
+
+}
+
 // This is expected to grow into some kind of engine / model for managing file conversions and giving in the moment
 // answers about conversions based on the users choices and enabled conversions.
 class FileConverter {
-
-    private struct Conversion {
-        let matches: (FileServer.DirectoryEntry) -> Bool
-        let filename: (FileServer.DirectoryEntry) -> String
-        let perform: (URL, URL) throws -> URL
-    }
 
     static let convertFiles: (FileServer.DirectoryEntry, URL) throws -> URL = { entry, url in
         guard let converter = converter(for: entry) else {
@@ -44,63 +179,17 @@ class FileConverter {
         return url
     }
 
-    private static let converters: [Conversion] = [
-
-        // Multiple Bitmap Images (*.mbm).
-        Conversion { entry in
-            return entry.fileType == .mbm || entry.pathExtension.lowercased() == "mbm"
-        } filename: { entry in
-            return entry.name
-                .deletingPathExtension
-                .appendingPathExtension("tiff")
-        } perform: { sourceURL, destinationURL in
-            let outputURL = destinationURL.appendingPathComponent(sourceURL.lastPathComponent.deletingPathExtension,
-                                                                  conformingTo: .tiff)
-            try PsiLuaEnv().convertMultiBitmap(at: sourceURL, to: outputURL)
-            try FileManager.default.removeItem(at: sourceURL)
-            return outputURL
-        },
-
-        // Psion Word (*.wrd).
-        Conversion { entry in
-            return entry.pathExtension.lowercased() == "wrd"
-        } filename: { entry in
-            return entry
-                .name
-                .deletingPathExtension
-                .appendingPathExtension("txt")
-        } perform: { sourceURL, destinationURL in
-            let outputURL = destinationURL.appendingPathComponent(sourceURL.lastPathComponent.deletingPathExtension,
-                                                                  conformingTo: .plainText)
-            let data = try Data(contentsOf: sourceURL)
-            let output = try PsionWord.processFile(data).get()
-            try output.write(to: outputURL, atomically: true, encoding: .utf8)
-            return outputURL
-        },
-
-        // Text files (*.txt, *.md). Convert Windows line endings to Unix line endings.
-        Conversion { entry in
-            return entry.pathExtension.lowercased() == "txt" || entry.pathExtension.lowercased() == "md"
-        } filename: { entry in
-            return entry.name
-        } perform: { sourceURL, destinationURL in
-            let outputURL = destinationURL.appendingPathComponent(sourceURL.lastPathComponent.deletingPathExtension,
-                                                                  conformingTo: .plainText)
-            let data = try Data(contentsOf: sourceURL)
-            guard let contents = String(data: data, encoding: .ascii) else {
-                throw ReconnectError.unknown
-            }
-            let output = contents.replacingOccurrences(of: "\r\n", with: "\n")
-            try output.write(to: outputURL, atomically: true, encoding: .utf8)
-            return outputURL
-        },
-
+    static let converters: [FileType: ConversionIdentifier] = [
+        .mbm: .mbmToTiff,
+        .word: .wordToText,
+        .text: .windowsAsciiToUnixUnicode,
+        .markdown: .windowsAsciiToUnixUnicode,
     ]
 
     private static func converter(for directoryEntry: FileServer.DirectoryEntry) -> Conversion? {
         return converters.first {
-            $0.matches(directoryEntry)
-        }
+            $0.key.matches(directoryEntry: directoryEntry)
+        }?.value.conversion
     }
 
     static func targetFilename(for directoryEntry: FileServer.DirectoryEntry) -> String {
